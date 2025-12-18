6b1f78dde202ec7482f7a4836dd79b93de1dfcc6
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-12-18 18:11:28 +0800

    Update x-cf.sh

--- x-cf.sh
+++ x-cf.sh
@@ -58,7 +58,7 @@ V6=""
 if [ ! -f xray ]; then
   echo "[+] 下载 Xray"
   curl $V6 -L -o xray.zip \
-    "https://download.liuyuan920907.workers.dev/xray-${XRAY_ARCH}"
+    "https://download.lycn.qzz.io/xray-${XRAY_ARCH}"
   unzip -q xray.zip xray
   chmod +x xray
   rm -f xray.zip
@@ -112,7 +112,7 @@ sleep 1
 if [ ! -f cloudflared ]; then
   echo "[+] 下载 cloudflared"
   curl $V6 -L -o cloudflared \
-    "https://download.liuyuan920907.workers.dev/cloudflare-${CF_ARCH}"
+    "https://download.lycn.qzz.io/cloudflare-${CF_ARCH}"
   chmod +x cloudflared
 fi
 
