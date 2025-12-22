b03e4fbe0955f85dd164c6243ca29edf05aa0670
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-12-22 18:00:07 +0800

    Update x-cf.sh

--- x-cf.sh
+++ x-cf.sh
@@ -221,9 +221,7 @@ cat > config.json <<EOF
   "dns": {
     "servers": ["8.8.8.8", "1.1.1.1"]
   },
-  "warp": {
-    "enabled": true
-  },
+
   "inbounds": [
     {
       "listen": "$LISTEN_ADDR",
