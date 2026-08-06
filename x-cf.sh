f43efd7e6382af73d07a21989517c5734bb89f23
ly921002 <92194283+ly921002@users.noreply.github.com>
2026-08-06 09:53:25 +0800

    Update x-cf.sh

--- x-cf.sh
+++ x-cf.sh
@@ -110,7 +110,7 @@ xray_vlessenc() {
 
 generate_pin() {
     PIN_CERT=$(openssl s_client \
-    -connect "${DOMAIN}:${PORT}" \
+    -connect "${DOMAIN}:443" \
     -servername "${DOMAIN}" </dev/null 2>/dev/null \
     | openssl x509 -pubkey -noout \
     | openssl pkey -pubin -outform der \
